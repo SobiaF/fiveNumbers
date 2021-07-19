cf73242a7e7d28414457578d81531fc03a8e6390
Sobia Furqan <sobiafurqan@hotmail.com>
2021-07-19 20:41:07 +0100

    formatting and minor updates

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-const fiveNumbers = [1,25,10,5,7]
-const random = Math.floor(Math.random() * fiveNumbers.length);
-console.log(fiveNumbers[random]);
-const selectNumber = fiveNumbers[random];
+const fiveNumbers = [1, 25, 10, 5, 7];
 
-let guessesRemaining  = 3;
+const randomNumber = Math.floor(Math.random() * fiveNumbers.length);
+console.log(fiveNumbers[randomNumber]);
+
+const selectedNumber = fiveNumbers[randomNumber];
+
+let guessesRemaining = 3;
 
 function checkGuess() {
-    let myGuess = parseInt (guess.value);
-    console.log(myGuess, selectNumber);
-    if (myGuess ===  selectNumber) {
+    const myGuess = parseInt(guess.value);
+    console.log(myGuess, selectedNumber);
+    if (myGuess === selectedNumber) {
         feedback.textContent = "Yay you got the number right!";
-    } else if (myGuess > selectNumber) {
-        feedback.textContent = myGuess + " smaller";
-    } else if (myGuess < selectNumber) {
-        feedback.textContent = myGuess + " larger";
+    } else if (myGuess > selectedNumber) {
+        feedback.textContent = "smaller";
+    } else if (myGuess < selectedNumber) {
+        feedback.textContent = "larger";
     }
     guessesRemaining--;
     if (guessesRemaining < 0) {
         feedback.textContent = "You're out of chances. The number was "
-            + selectNumber;
+            + selectedNumber;
     }
-    console.log(guessesRemaining)
+    console.log(guessesRemaining);
 }
